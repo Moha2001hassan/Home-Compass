a2bd0c7cbcb79234319f5e6a9a01e186c7c6babe
Mohamed Hassan <mohahassanna933@gmail.com>
2024-01-21 18:31:08 +0200

    test

---
+++ app/src/main/java/com/mohassan/homecompass/home_compass_feature/presentation/viewmodels/SearchMissingViewModel.kt
@@ -0,0 +1,13 @@
+package com.mohassan.homecompass.home_compass_feature.presentation.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SearchMissingViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
