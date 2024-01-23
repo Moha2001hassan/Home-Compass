e670a65921fe75e7661720209e35471ee72f0951
Nadine Islam <nadeen.kamaal@gmail.com>
2024-01-23 14:09:51 +0200

    Implemented fragment_setting.xml

--- app/src/main/java/com/mohassan/homecompass/home_compass_feature/presentation/fragments/SettingFragment.kt
+++ app/src/main/java/com/mohassan/homecompass/home_compass_feature/presentation/fragments/SettingFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.mohassan.homecompass.databinding.FragmentSettingBinding
@@ -29,10 +28,10 @@ class SettingFragment : Fragment() {
         _binding = FragmentSettingBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        settingViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textSlideshow
+//        settingViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
