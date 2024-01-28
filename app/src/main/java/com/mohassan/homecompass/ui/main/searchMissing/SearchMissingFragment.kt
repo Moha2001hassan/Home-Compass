1e739ff0455e9d4274c38c4f5112582a2fc96bf6
Nadine Islam <nadeen.kamaal@gmail.com>
2024-01-28 15:59:59 +0200

    Implemented SearchMissingFragment

--- app/src/main/java/com/mohassan/homecompass/ui/main/searchMissing/SearchMissingFragment.kt
+++ app/src/main/java/com/mohassan/homecompass/ui/main/searchMissing/SearchMissingFragment.kt
@@ -4,39 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.mohassan.homecompass.databinding.FragmentSearchMissingBinding
 
 class SearchMissingFragment : Fragment() {
 
-    private var _binding: FragmentSearchMissingBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentSearchMissingBinding
+    private val galleryLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) {_ ->
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val searchMissingViewModel =
-            ViewModelProvider(this)[SearchMissingViewModel::class.java]
 
-        _binding = FragmentSearchMissingBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        binding = FragmentSearchMissingBinding.inflate(inflater)
+        return binding.root
+    }
 
-        val textView: TextView = binding.textSlideshow
-        searchMissingViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnUploadPhoto.setOnClickListener {
+            galleryLauncher.launch("image/*")
         }
-        return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
