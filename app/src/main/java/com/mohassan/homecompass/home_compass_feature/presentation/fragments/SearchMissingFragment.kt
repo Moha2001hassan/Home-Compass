0832e291f53e288329bb6e0f8e499f1598271587
Nadine Islam <nadeen.kamaal@gmail.com>
2024-06-22 13:30:56 +0300

    Added feature to select and display photo with associated data

--- app/src/main/java/com/mohassan/homecompass/home_compass_feature/presentation/fragments/SearchMissingFragment.kt
+++ app/src/main/java/com/mohassan/homecompass/home_compass_feature/presentation/fragments/SearchMissingFragment.kt
@@ -1,18 +1,28 @@
 package com.mohassan.homecompass.home_compass_feature.presentation.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.mohassan.homecompass.databinding.FragmentSearchMissingBinding
+import com.mohassan.homecompass.home_compass_feature.presentation.ImageDisplayActivity
 
 class SearchMissingFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchMissingBinding
     private val galleryLauncher =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { _ ->
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            showLoadingProgressBar()
+            Handler(Looper.getMainLooper()).postDelayed({
+                hideLoadingProgressBar()
+                navigateToImageDisplay(uri)
+            }, 5000)
         }
 
     override fun onCreateView(
@@ -31,5 +41,23 @@ class SearchMissingFragment : Fragment() {
             galleryLauncher.launch("image/*")
         }
     }
+    private fun showLoadingProgressBar() {
+        // Show your loading progress bar
+        // For simplicity, let's assume you have a ProgressBar in your layout
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideLoadingProgressBar() {
+        // Hide your loading progress bar
+        binding.progressBar.visibility = View.GONE
+    }
+    private fun navigateToImageDisplay(imageUri: Uri?) {
+        // Navigate to ImageDisplayActivity to show the selected image
+        val intent = Intent(requireContext(), ImageDisplayActivity::class.java)
+        intent.putExtra(ImageDisplayActivity.EXTRA_IMAGE_URI, imageUri.toString())
+        startActivity(intent)
+    }
+
+
 
 }
